2cb482b82d1591c18e4bf46b4919e1b66fe7e22d
David <borjacazalesdavid@gmail.com>
2022-06-17 23:56:30 -0500

    fix(test):

--- app/tests/test_service_shopping/test_shopping.py
+++ app/tests/test_service_shopping/test_shopping.py
@@ -1,4 +1,4 @@
-def test_service_shopping(client):
+def test_service_shopping_with_parameter(client):
     """
     test service get statistics
     """
@@ -10,3 +10,30 @@ def test_service_shopping(client):
     print(response.data)
     print(response.status)
     assert response.status == "200 OK"
+
+
+def test_service_shopping_without_parameter(client):
+    """
+    test service get statistics
+    """
+    mimetype = "application/json"
+    headers = {"Content-Type": mimetype, "Accept": mimetype}
+    url = "/api/v1/shopping/statistics"
+    response = client.get(url, headers=headers)
+    print(response.data)
+    print(response.status)
+    assert response.status == "200 OK"
+
+
+def test_service_shopping_bad_parameters(client):
+    """
+    test service get statistics bad parameters
+    """
+    mimetype = "application/json"
+    headers = {"Content-Type": mimetype, "Accept": mimetype}
+    data = "test"
+    url = f"/api/v1/shopping/statistics?categor={data}"
+    response = client.get(url, headers=headers)
+    print(response.data)
+    print(response.status)
+    assert response.status == "400 BAD REQUEST"
